f6ce5cbb5ddfa029d8a5b8bcd0105e0d3d70110e
jaenudin <jaenudinudin84@gmail.com>
2026-01-24 13:57:09 +0700

    qw

--- brodher_product_serial/__manifest__.py
+++ brodher_product_serial/__manifest__.py
@@ -45,7 +45,7 @@
         # 'wizard/stock_picking_sn_wizard_views.xml',  # NEW
         'wizard/brodher_stock_picking_sn_wizard_views.xml',  # NEW
          'reports/serial_number_qrcode_report.xml',  # ← TAMBAHKAN INI
-         'report/sn_status_report.xml',
+         'reports/sn_status_report.xml',
          'wizard/sn_status_report_wizard_views.xml',
          'views/sn_status_report_menu.xml',
 
