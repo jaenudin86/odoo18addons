ff908a731a396f3f85203192a33151ba902437d2
jaenudin86 <jaenudinudin84@gmail.com>
2026-02-11 07:46:04 +0700

    12

--- pos_warehouse_access/models/__init__.py
+++ pos_warehouse_access/models/__init__.py
@@ -5,5 +5,6 @@ from . import res_config_settings
 from . import pos_config
 from . import stock_warehouse
 from . import stock_location
+from . import stock_picking_type
 from . import stock_picking
 from . import stock_quant
--- pos_warehouse_access/models/stock_picking_type.py
+++ pos_warehouse_access/models/stock_picking_type.py
@@ -1,51 +1,32 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, api
 
 
-class StockWarehouse(models.Model):
-    _inherit = 'stock.warehouse'
+class StockPickingType(models.Model):
+    _inherit = 'stock.picking.type'
 
-    user_access_ids = fields.Many2many(
-        'res.users',
-        'warehouse_user_access_rel',
-        'warehouse_id',
-        'user_id',
-        string='User Access',
-        help='User yang bisa melihat gudang ini'
-    )
-    
-    user_access_count = fields.Integer(
-        string='User Access Count',
-        compute='_compute_user_access_count'
-    )
-    
-    @api.depends('user_access_ids')
-    def _compute_user_access_count(self):
-        for warehouse in self:
-            warehouse.user_access_count = len(warehouse.user_access_ids)
-    
     @api.model
     def search(self, args, offset=0, limit=None, order=None):
-        """Override search to filter warehouses based on user access"""
+        """Override search to filter picking types based on warehouse access"""
         # Check if user is Stock Manager or Admin
         if not self.env.user.has_group('stock.group_stock_manager') and \
            not self.env.user.has_group('base.group_system'):
             # Regular user - filter by warehouse access
             allowed_warehouse_ids = self.env.user.warehouse_access_ids.ids
             if allowed_warehouse_ids:
-                args = ['&', ('id', 'in', allowed_warehouse_ids)] + args
+                args = ['&', ('warehouse_id', 'in', allowed_warehouse_ids)] + args
             else:
                 # No access to any warehouse
                 args = [('id', 'in', [])] + args
         
-        return super(StockWarehouse, self).search(
+        return super(StockPickingType, self).search(
             args, offset=offset, limit=limit, order=order
         )
-    
+
     @api.model
     def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
-        """Override search_read to filter warehouses based on user access"""
+        """Override search_read to filter picking types based on warehouse access"""
         if domain is None:
             domain = []
         
@@ -55,18 +36,18 @@ class StockWarehouse(models.Model):
             # Regular user - filter by warehouse access
             allowed_warehouse_ids = self.env.user.warehouse_access_ids.ids
             if allowed_warehouse_ids:
-                domain = ['&', ('id', 'in', allowed_warehouse_ids)] + domain
+                domain = ['&', ('warehouse_id', 'in', allowed_warehouse_ids)] + domain
             else:
                 # No access to any warehouse
                 domain = [('id', 'in', [])] + domain
         
-        return super(StockWarehouse, self).search_read(
+        return super(StockPickingType, self).search_read(
             domain=domain, fields=fields, offset=offset, limit=limit, order=order
         )
-    
+
     @api.model
     def web_search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
-        """Override web_search_read for web client kanban/list views"""
+        """Override web_search_read for kanban dashboard"""
         if domain is None:
             domain = []
         
@@ -76,23 +57,11 @@ class StockWarehouse(models.Model):
             # Regular user - filter by warehouse access
             allowed_warehouse_ids = self.env.user.warehouse_access_ids.ids
             if allowed_warehouse_ids:
-                domain = ['&', ('id', 'in', allowed_warehouse_ids)] + domain
+                domain = ['&', ('warehouse_id', 'in', allowed_warehouse_ids)] + domain
             else:
                 # No access to any warehouse
                 domain = [('id', 'in', [])] + domain
         
-        return super(StockWarehouse, self).web_search_read(
+        return super(StockPickingType, self).web_search_read(
             domain=domain, fields=fields, offset=offset, limit=limit, order=order
         )
-    
-    def action_view_user_access(self):
-        """Open list view of users that can access this warehouse"""
-        self.ensure_one()
-        return {
-            'name': 'User Access',
-            'type': 'ir.actions.act_window',
-            'res_model': 'res.users',
-            'view_mode': 'list,form',
-            'domain': [('id', 'in', self.user_access_ids.ids)],
-            'context': {'default_warehouse_access_ids': [(4, self.id)]},
-        }
