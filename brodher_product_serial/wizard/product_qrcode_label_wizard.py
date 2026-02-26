0fb77612ed385899992b492046dec42b58ef45a8
jaenudin86 <jaenudinudin84@gmail.com>
2026-02-27 06:37:20 +0700

    1

--- brodher_product_serial/wizard/product_qrcode_label_wizard.py
+++ brodher_product_serial/wizard/product_qrcode_label_wizard.py
@@ -1,14 +1,20 @@
 from odoo import models, fields, api
 
-
 class ProductQrcodeLabelWizard(models.TransientModel):
     _name = 'product.qrcode.label.wizard'
     _description = 'Product QR Code Label Wizard'
 
     product_ids = fields.Many2many(
         'product.product',
+        'wizard_product_rel',
         string='Products',
         required=True
+    )
+    variant_ids = fields.Many2many(
+        'product.product',
+        'wizard_variant_rel',
+        string='Variants to Print',
+        domain="[('id', 'in', product_ids)]",
     )
     quantity = fields.Integer(
         string='Quantity per Product',
@@ -22,17 +28,16 @@ class ProductQrcodeLabelWizard(models.TransientModel):
         res = super().default_get(fields_list)
         active_model = self.env.context.get('active_model')
         active_ids = self.env.context.get('active_ids', [])
-        
         if active_model == 'product.product' and active_ids:
             res['product_ids'] = [(6, 0, active_ids)]
+            res['variant_ids'] = [(6, 0, active_ids)]  # default semua terpilih
         elif active_model == 'product.template' and active_ids:
             templates = self.env['product.template'].browse(active_ids)
             variant_ids = templates.mapped('product_variant_ids').ids
             res['product_ids'] = [(6, 0, variant_ids)]
-        
+            res['variant_ids'] = [(6, 0, variant_ids)]  # default semua terpilih
         return res
 
     def action_print_labels(self):
-        """Generate and print QR code labels"""
         self.ensure_one()
         return self.env.ref('brodher_product_serial.action_report_product_qrcode_label').report_action(self)
