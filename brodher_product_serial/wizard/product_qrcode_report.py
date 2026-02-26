91985afd6ccb7f13f0ba0f9cac1314401f9ed9b2
jaenudin86 <jaenudinudin84@gmail.com>
2026-02-27 06:35:38 +0700

    12

--- brodher_product_serial/wizard/product_qrcode_report.py
+++ brodher_product_serial/wizard/product_qrcode_report.py
@@ -4,15 +4,17 @@ class ProductQrcodeReport(models.AbstractModel):
     _name = 'report.brodher_product_serial.product_qrcode_label_template'
     _description = 'Product QR Code Label Report'
 
+    
     @api.model
     def _get_report_values(self, docids, data=None):
         docs = self.env['product.qrcode.label.wizard'].browse(docids)
         
         label_data = []
         for doc in docs:
-            for product in doc.product_ids:
+            # pakai variant_ids kalau ada, fallback ke product_ids
+            products = doc.variant_ids if doc.variant_ids else doc.product_ids
+            for product in products:
                 for i in range(doc.quantity):
-                    # Ambil variant attributes
                     variant_values = product.product_template_attribute_value_ids.mapped('name')
                     variant_str = ' / '.join(variant_values) if variant_values else ''
                     
