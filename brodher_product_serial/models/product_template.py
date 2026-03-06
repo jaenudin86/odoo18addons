d548528e4fe644ed1ab0b583019dca694cacfe19
jaenudin <jaenudin@voksel.co.id>
2026-03-06 17:23:41 +0700

    987

--- brodher_product_serial/models/product_template.py
+++ brodher_product_serial/models/product_template.py
@@ -8,7 +8,7 @@ class ProductTemplate(models.Model):
     sn_product_type = fields.Selection([
         ('M', 'Man'),
         ('W', 'Woman')
-    ], string='Product Type', default='M')
+    ], string='Products Type', default='M')
     
     serial_number_ids = fields.One2many(
         'stock.lot', 'product_id', string='Serial Numbers',
