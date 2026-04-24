37e05bc3adc24dfd976febff6b2e60873617cc6d
jaenudin <jaenudin@voksel.co.id>
2026-04-24 13:32:28 +0700

    1

--- brodher_product_serial/__manifest__.py
+++ brodher_product_serial/__manifest__.py
@@ -34,6 +34,7 @@
         'wizard/scan_sn_out_wizard_views.xml',     # BARU
         'wizard/sn_validation_wizard_views.xml',
         'views/product_template_views.xml',
+        'views/purchase_order_views.xml',
          'reports/sn_print_report.xml',
           'views/stock_picking_views.xml',
         'views/stock_lot_views.xml',
--- brodher_product_serial/models/purchase_order.py
+++ brodher_product_serial/models/purchase_order.py
@@ -3,7 +3,21 @@ from odoo import models, fields, api, _
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
+
+    state = fields.Selection(selection_add=[('closed', 'Closed')], ondelete={'closed': 'set default'})
     
+    def action_close_po(self):
+        for order in self:
+            if order.state == 'purchase':
+                # Cancel associated pickings that are not finished
+                pickings_to_cancel = order.picking_ids.filtered(lambda p: p.state not in ('done', 'cancel'))
+                if pickings_to_cancel:
+                    pickings_to_cancel.action_cancel()
+                    order.message_post(body=_("Associated transfers %s have been cancelled due to PO closure.") % 
+                                       (", ".join(pickings_to_cancel.mapped('name'))))
+                
+                order.write({'state': 'closed'})
+
     def button_confirm(self):
         res = super(PurchaseOrder, self).button_confirm()
         has_sn_product = any(
@@ -16,4 +30,4 @@ class PurchaseOrder(models.Model):
                 'Please scan Serial Numbers during receipt.'
             )
             self.message_post(body=message, message_type='notification')
-        return res
+        return res
